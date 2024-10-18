95ec4cc84a9a51fd8d2656eab8b5a4c4ddd1cd40
Ethan Dean <ethandean7@gmail.com>
2024-10-18 12:34:52 -0400

    Initial commit

---
+++ backend/src/index.ts
@@ -0,0 +1,12 @@
+import express, { Application } from "express";
+
+const app: Application = express();
+const port: number = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello from TypeScript and Express!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
