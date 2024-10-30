cad3f7f563b413802a8d0334409ef11d928e4aba
Ethan Dean <ethandean7@gmail.com>
2024-10-30 19:38:32 -0400

    Fix what i broke

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,7 +6,7 @@ import "./index.css";
 import createRouter from "./router";
 
 // Create
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <RouterProvider router={createRouter()} />
   </React.StrictMode>,
