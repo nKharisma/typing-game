3afe658ede73ee4a36b8e74760cb6bbee5ddfa41
Ethan Dean <ethandean7@gmail.com>
2024-12-02 19:15:54 -0500

    Add profile page basics

--- frontend/src/pages/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage.tsx
@@ -1,13 +1,14 @@
 import { useContext } from "react";
 import { AuthContext } from "../contexts/AuthContext";
 
+import '../css/ProfilePage.css'
+
 export default function ProfilePage() {
   const { logout } = useContext(AuthContext);
 
   return (
     <div className='profile-page'>
-      <button onClick={logout}>Log out</button>
-      <p>This is the "protected" profile page</p>
+      <button className='profile-page__button' onClick={logout}>Log out</button>
     </div>
   );
 }
