3fd656344279169be08fb27c1fcaa26d1fec46f8
Ethan Dean <ethandean7@gmail.com>
2024-12-02 19:05:32 -0500

    Add HomePage and AboutPage, and match all CSS

--- frontend/src/pages/AboutPage.tsx
+++ frontend/src/pages/AboutPage.tsx
@@ -1,7 +1,36 @@
+import '../css/AboutPage.css'
+
 export default function AboutPage() {
   return (
     <div className='about-page'>
-      <p>This is the about page</p>
+      <div id="about-us">
+        <h1>About Us</h1>
+        <p>
+          Welcome to the TypeCode website!
+          This platform is designed to help you manage build coding muscle memory efficiently and with fun.
+        </p>
+        <h2>Our Team</h2>
+        <ul>
+          <li>
+            <strong>Reed Holton</strong> - Project Manager
+          </li>
+          <li>
+            <strong>SeNiah McField</strong> - Frontend Developer
+          </li>
+          <li>
+            <strong>Ethan Dean</strong> - Frontend Developer
+          </li>
+          <li>
+            <strong>Terry Achille</strong> - API Developer
+          </li>
+          <li>
+            <strong>Marco Plasencia</strong> - API Developer
+          </li>
+          <li>
+            <strong>Wesley Underwood</strong> - Database Developer
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,7 +1,22 @@
+import '../css/HomePage.css'
+
 export default function HomePage() {
   return (
-    <div className='home-page'>
-      <p>This is the home page</p>
+    <div className='home-page__container'>
+      <div className='home-page'>
+        <div className='home-page__block-1'>
+          <div className='home-page__block-1-text-container'>
+            <h1>Welcome to TypeCode!</h1>
+            <p>
+              Our mission is to help you build muscle memory for coding,
+              and to have fun while doing it!
+            </p>
+          </div>
+          <div className='home-page__block-1-image-container'>
+            <img className='home-page__block-1-image' src="assets/tile022_scaled.png" alt="Spaceship" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
