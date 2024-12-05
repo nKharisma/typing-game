132085f7d0456e632028a4a05fb0f9a875934ace
Terry Achille <achille.terry@gmail.com>
2024-12-05 15:05:21 -0500

    loops puzzle

---
+++ frontend/temp_code_levels/java/Loops.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+class Loops {
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Your spaceship requires energy crystals to power up for the journey ahead.
+        // You must mine asteroids to gather as many energy crystals as possible!
+
+        // Instructions
+        // Modify this program to calculate the total number of energy crystals collected.
+        // Use a loop to collect crystals from each asteroid.
+
+        // Input
+        // The total number of asteroids 'N'
+        // For each asteroid, the number of energy crystals it contains
+
+        // Output
+        // The total number of energy crystals collected
+
+        // Getting the number of asteroids
+        int n = scanner.nextInt();
+
+        int totalCrystals = 0; // Variable to store the total number of crystals
+        
+        // --- User Submission Area ---
+        // Loop to get the number of crystals from each asteroid
+
+        
+        // --- End of User Submission Area ---
+        
+        // Closing scanner
+        scanner.close();
+    }
+}
---
+++ frontend/temp_code_levels/java/LoopsAnswer.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+class LoopsAnswer {
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Your spaceship requires energy crystals to power up for the journey ahead.
+        // You must mine asteroids to gather as many energy crystals as possible!
+
+        // Instructions
+        // Modify this program to calculate the total number of energy crystals collected.
+        // Use a loop to collect crystals from each asteroid.
+
+        // Input
+        // The total number of asteroids 'N'
+        // For each asteroid, the number of energy crystals it contains
+
+        // Output
+        // The total number of energy crystals collected
+
+        // Getting the number of asteroids
+        int n = scanner.nextInt();
+
+        int totalCrystals = 0; // Variable to store the total number of crystals
+        
+        // --- User Submission Area ---
+        // Loop to get the number of crystals from each asteroid
+        // Answer!!
+        for (int i = 0; i < n; i++) {
+            int crystals = scanner.nextInt();
+
+            // Add the number of crystals from the current asteroid to the total
+            totalCrystals += crystals;
+        }
+
+        
+        // Output the total number of energy crystals collected
+        System.out.println(totalCrystals);
+        // --- End of User Submission Area ---
+        
+        // Closing scanner
+        scanner.close();
+    }
+}
