b5b923aafd5e4124c73a39a2c9675e3039a95ca8
Ethan Dean <ethandean7@gmail.com>
2024-12-06 01:58:52 -0500

    Add expected results fetch

--- frontend/src/components/ConsoleOutput.tsx
+++ frontend/src/components/ConsoleOutput.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 
 interface ConsoleOutputProps {
+  expectedOutput: string;
 	userOutput: string;
 }
 
-const ConsoleOutput: React.FC<ConsoleOutputProps> = ({ userOutput }) => {
+const ConsoleOutput: React.FC<ConsoleOutputProps> = ({ expectedOutput, userOutput }) => {
 	return (
 		<div>
-			<h3>Console Output</h3>
-			<pre>{userOutput}</pre>
+			<h4>Expected Output</h4>
+			<p>{expectedOutput}</p>
+      <br/>
+			<h4>User Output</h4>
+			<p>{userOutput}</p>
 		</div>
 	);
 };
--- frontend/src/pages/GameLevelOne.tsx
+++ frontend/src/pages/GameLevelOne.tsx
@@ -1,4 +1,4 @@
-import React, { useState, } from 'react';
+import React, { useState, useEffect } from 'react';
 import ConsoleOutput from '../components/ConsoleOutput';
 import CodeEditor from '../components/CodeEditor';
 import GameDescription from '../components/GameDescription';
@@ -7,6 +7,28 @@ import '../css/puzzleLayout.css';
 
 const GameLevelOne: React.FC = () => {
   const [userOutput, setUserOutput] = useState('');
+  const [expectedOutput, setExpectedOutput] = useState('');
+
+  useEffect(() => {
+    const getExpectedOutput = async () => {
+      try {
+        const response = await fetch(`${getBackendUrl()}/api/v1/user/get-expected-output`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            name: 'variables',
+          }),
+        });
+        const data = await response.json();
+        setExpectedOutput(data.output || data.error);
+      } catch (error) {
+        setExpectedOutput('Error contacting server...');
+      }
+    };
+    getExpectedOutput();
+  }, []);
 
   const handleRunCode = async (code: string) => {
     try {
@@ -17,7 +39,7 @@ const GameLevelOne: React.FC = () => {
         },
         body: JSON.stringify({
           language: 'java',
-          name: "variables",
+          name: 'variables',
           code: code,
         }),
       });
@@ -34,7 +56,7 @@ const GameLevelOne: React.FC = () => {
         <GameDescription language="java" filename="Variables"/>
       </div>
         <div className="bottom-left">
-        <ConsoleOutput userOutput={userOutput} />
+        <ConsoleOutput expectedOutput={expectedOutput} userOutput={userOutput} />
       </div>
       <div className="right">
         <CodeEditor language="java" theme="monokai" filename="Variables" onRunCode={handleRunCode} />
