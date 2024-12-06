32201ac4401c1de1e85b157f0ad8dd8329bee1a3
Terry Achille <achille.terry@gmail.com>
2024-12-06 02:56:00 -0500

    Fix for empty outputs

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -565,6 +565,9 @@ expressServer.post('/api/v1/user/compile', async (req: any, res: any) => {
 
     if(response.data.run.stdout.trim() == getTestCaseResult.expectedOutput){
       res.status(200).json({ status: "PASS", output: response.data.run.stdout, expected: getTestCaseResult.expectedOutput });
+    } 
+    else if (!response.data.run.output){
+      res.status(400).json({ status: "FAIL", output: "<EMPTY_OUTPUT>", expected: getTestCaseResult.expectedOutput });
     } else {
       res.status(400).json({ status: "FAIL", output: response.data.run.output, expected: getTestCaseResult.expectedOutput });
     }
